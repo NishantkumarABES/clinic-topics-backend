f33288e4e1a0be944ca4c92d7ce3e1e2024747ee
Nishant Kumar <nishant.kumar@qsstechnosoft.com>
2026-01-09 14:14:21 +0530

    minor changes 2

--- apps/commerce/views.py
+++ apps/commerce/views.py
@@ -2,7 +2,7 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny, IsAuthenticated
-from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.parsers import MultiPartParser, FormParser, JSONParser
 from rest_framework.pagination import PageNumberPagination
 from drf_yasg.utils import swagger_auto_schema
 from django.shortcuts import get_object_or_404
@@ -82,7 +82,7 @@ class CartDetailView(APIView):
 
 class AddToCartView(APIView):
     permission_classes = [IsAuthenticated]
-    parser_classes = [MultiPartParser, FormParser]
+    parser_classes = [JSONParser]
 
     @swagger_auto_schema(request_body=AddToCartSerializer(), responses={201: "Item added to cart"})
     def post(self, request):
