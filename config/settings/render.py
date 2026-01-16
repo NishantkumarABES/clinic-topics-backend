98ab0422301e52d781b3cbd8a15af06bf989a817
Nishant Kumar <nishant.kumar@qsstechnosoft.com>
2026-01-16 23:05:01 +0530

    storage change issue fix

--- config/settings/render.py
+++ config/settings/render.py
@@ -20,9 +20,11 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
+
+
 
 
 
