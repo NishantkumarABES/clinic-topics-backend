0e74628d7eae0b4c3dcf1f55f8a39d4f7fc260b4
Nishant Kumar <nishant.kumar@qsstechnosoft.com>
2026-01-22 16:20:59 +0530

    build issue

--- apps/report_template/services.py
+++ apps/report_template/services.py
@@ -1,33 +1,12 @@
-import os
+import os, tempfile
 from django.template import Template, Context
-from weasyprint import HTML
+if os.getenv('ENVIRONMENT') == 'development':
+    from pyhtml2pdf import converter
+else: from weasyprint import HTML
 from django.conf import settings
 import datetime
 
 from apps.report_template.templates.report import get_report_template_string
-
-
-# class ReportPDFService:
-#     @staticmethod
-#     def generate_pdf(template_data: dict) -> str:
-#         template_string = get_report_template_string()  
-#         template = Template(template_string)
-#         html_content = template.render(Context(template_data))
-
-#         # Create temporary HTML file
-#         with tempfile.NamedTemporaryFile(delete=False, suffix=".html") as html_file:
-#             html_file.write(html_content.encode("utf-8"))
-#             html_path = html_file.name
-
-#         # Output PDF path
-#         pdf_filename = f"second_opinion_report_{template_data['patient_id']}.pdf"
-#         pdf_path = os.path.join(settings.MEDIA_ROOT, "reports", pdf_filename)
-#         os.makedirs(os.path.dirname(pdf_path), exist_ok=True)
-#         # Convert HTML → PDF
-#         converter.convert(f"file:///{html_path}", pdf_path)
-#         # Cleanup temp html
-#         os.remove(html_path)
-#         return pdf_path
 
 
 class ReportPDFService:
@@ -41,9 +20,16 @@ class ReportPDFService:
         pdf_filename = f"second_opinion_report_{template_data['patient_id']}.pdf"
         pdf_path = os.path.join(settings.MEDIA_ROOT, "reports", pdf_filename)
         os.makedirs(os.path.dirname(pdf_path), exist_ok=True)
+        if os.getenv('ENVIRONMENT') == 'development':
+            with tempfile.NamedTemporaryFile(delete=False, suffix=".html") as html_file:
+                html_file.write(html_content.encode("utf-8"))
+                html_path = html_file.name
 
-        # Generate PDF directly from HTML string
-        HTML(string=html_content, base_url=settings.MEDIA_ROOT).write_pdf(pdf_path)
+            converter.convert(f"file:///{html_path}", pdf_path)
+            os.remove(html_path)
+        else:
+            # Generate PDF directly from HTML string
+            HTML(string=html_content, base_url=settings.MEDIA_ROOT).write_pdf(pdf_path)
 
         return pdf_path
 
--- build.sh
+++ build.sh
@@ -3,7 +3,14 @@ set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
-python manage.py migrate --noinput
+python manage.py migrate --fake-initial --noinput
+
+
+# set -o errexit
+
+# pip install -r requirements.txt
+# python manage.py collectstatic --noinput
+# python manage.py migrate --noinput
 
 # python manage.py runserver 0.0.0.0:8000
 # uvicorn config.asgi:application --host 0.0.0.0 --port 8000
