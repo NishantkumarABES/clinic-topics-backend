bb2472f2332b718106e478e9eda78435a41908ae
Nishant Kumar <nishant.kumar@qsstechnosoft.com>
2026-01-24 01:33:31 +0530

    db expires new DB

--- build.sh
+++ build.sh
@@ -1,9 +1,26 @@
 #!/usr/bin/env bash
+
 set -o errexit
 
 pip install -r requirements.txt
+
+# Remove old migration files (keeps __init__.py)
+find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
+find . -path "*/migrations/*.pyc" -delete
+
+# Recreate fresh migrations from current models
+python manage.py makemigrations
+
+# Collect static files
 python manage.py collectstatic --noinput
+
+# Apply migrations
 python manage.py migrate --noinput
+# set -o errexit
+
+# pip install -r requirements.txt
+# python manage.py collectstatic --noinput
+# python manage.py migrate --noinput
 
 # python manage.py runserver 0.0.0.0:8000
 # uvicorn config.asgi:application --host 0.0.0.0 --port 8000
