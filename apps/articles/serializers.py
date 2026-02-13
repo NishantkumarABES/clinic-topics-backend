5e7a56957655f92e552b7b76dad74959bdedaf18
Nishant Kumar <nishant.kumar@qsstechnosoft.com>
2026-02-13 12:12:24 +0530

    articles app api

--- apps/articles/serializers.py
+++ apps/articles/serializers.py
@@ -1,56 +1,131 @@
 from rest_framework import serializers
 from apps.articles.models import Article, Bookmark
-
+from apps.articles.constants import Status
+from apps.accounts.constants import UserRole
 
 class ArticleListSerializer(serializers.ModelSerializer):
     is_bookmarked = serializers.SerializerMethodField()
+    uploaded_by = serializers.StringRelatedField()
 
     class Meta:
         model = Article
         fields = [
             "id",
+            "uploaded_by",
             "title",
-            "slug",
             "article_type",
             "speciality",
             "authors",
             "institution",
-            "upload_date",
+            "publication_date",
+            "year",
             "view_count",
             "download_count",
             "status",
-            "is_featured",
             "is_bookmarked",
         ]
 
-    def get_is_bookmarked(self, obj):
-        user = self.context.get("user")
 
+    def get_is_bookmarked(self, obj):
+        user = self.context.get("request").user
         if not user or not user.is_authenticated:
             return False
-
-        return Bookmark.objects.filter(
-            user=user,
-            article=obj
-        ).exists()
+        return Bookmark.objects.filter(user=user, article=obj).exists()
 
 class ArticleDetailSerializer(serializers.ModelSerializer):
     is_bookmarked = serializers.SerializerMethodField()
+    uploaded_by = serializers.StringRelatedField()
 
     class Meta:
         model = Article
         fields = "__all__"
 
     def get_is_bookmarked(self, obj):
-        user = self.context.get("user")
-
+        user = self.context.get("request").user
         if not user or not user.is_authenticated:
             return False
+        return Bookmark.objects.filter(user=user, article=obj).exists()
 
-        return Bookmark.objects.filter(
-            user=user,
-            article=obj
-        ).exists()
+    def __init__(self, *args, **kwargs):
+        """
+        Hide moderation fields for non-admin users.
+        """
+        super().__init__(*args, **kwargs)
+        request = self.context.get("request")
+
+        if not request:
+            return
+
+        if request.user.role != UserRole.ADMIN:
+            self.fields.pop("is_deleted", None)
+
+class ArticleCreateSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Article
+        exclude = (
+            "uploaded_by",
+            "status",
+            "view_count",
+            "download_count",
+            "rejection_reason",
+            "is_deleted",
+            "created_at",
+            "updated_at",
+        )
+
+    # def validate_authors(self, value):
+    #     if not isinstance(value, list) or len(value) == 0:
+    #         raise serializers.ValidationError(
+    #             "Authors must be a non-empty list."
+    #         )
+    #     return value
+
+class ArticleUpdateSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Article
+        exclude = (
+            "uploaded_by",
+            "status",
+            "view_count",
+            "download_count",
+            "rejection_reason",
+            "is_deleted",
+            "created_at",
+            "updated_at",
+        )
+
+class ArticleReviewSerializer(serializers.ModelSerializer):
+
+    REVIEW_STATUS = (
+        (Status.PUBLISHED, "Publish"),
+        (Status.REJECTED, "Reject"),
+    )
+
+    status = serializers.ChoiceField(choices=REVIEW_STATUS)
+    rejection_reason = serializers.CharField(
+        required=False,
+        allow_blank=True
+    )
+
+    class Meta:
+        model = Article
+        fields = ("status", "rejection_reason")
+
+    def validate(self, attrs):
+        status_value = attrs.get("status")
+        reason = attrs.get("rejection_reason")
+
+        if status_value == Status.REJECTED and not reason:
+            raise serializers.ValidationError(
+                "Rejection reason is required when rejecting."
+            )
+
+        if status_value == Status.PUBLISHED:
+            attrs["rejection_reason"] = None
+
+        return attrs
 
 class BookmarkSerializer(serializers.ModelSerializer):
 
@@ -58,16 +133,10 @@ class BookmarkSerializer(serializers.ModelSerializer):
         model = Bookmark
         fields = ["id", "article", "created_at"]
 
+#####################  Response Serializers #############################
 
-################### Response Serializers ###################
-class ArticlePaginationSerializer(serializers.Serializer):
+class PaginatedArticlesListResponseSerializer(serializers.Serializer):
     count = serializers.IntegerField()
-    next = serializers.CharField(allow_null=True)
-    previous = serializers.CharField(allow_null=True)
+    next = serializers.URLField(allow_null=True)
+    previous = serializers.URLField(allow_null=True)
     results = ArticleListSerializer(many=True)
-
-
-class ArticleListResponseSerializer(serializers.Serializer):
-    details = serializers.CharField()
-    data = ArticlePaginationSerializer()
-    success = serializers.BooleanField()
--- apps/articles/urls.py
+++ apps/articles/urls.py
@@ -1,11 +1,25 @@
 from django.urls import path
 from apps.articles.views import (
-    ArticleListView, ArticleDetailView, ToggleBookmarkView, ArticleDownloadView
+    ArticleListView, ArticleDetailView, ToggleBookmarkView, ArticleCreateView, MyArticlesView, 
+    ArticleUpdateView, ArticleReviewView, AdminMoveToReviewView, AdminArticleListView,
+    BookmarkListView, SoftDeleteArticleView
 )
 
 urlpatterns = [
+    # public
     path("", ArticleListView.as_view(), name="article-list"),
-    path("<slug:slug>/", ArticleDetailView.as_view(), name="article-detail"),
-    path("<slug:slug>/bookmark/", ToggleBookmarkView.as_view(), name="article-bookmark"),
-    path("<slug:slug>/download/", ArticleDownloadView.as_view(), name="article-download"),
+    path("<uuid:id>/", ArticleDetailView.as_view(), name="article-detail"),
+    path("<uuid:id>/bookmark/", ToggleBookmarkView.as_view(), name="article-bookmark"),
+    path("create/", ArticleCreateView.as_view(), name="article-create"),
+
+    # Doctors
+    path("my/", MyArticlesView.as_view(), name="my-articles"),
+    path("<uuid:id>/update/", ArticleUpdateView.as_view(), name="article-update"),
+    path("<uuid:id>/delete/", SoftDeleteArticleView.as_view(), name="article-delete"),
+    path("bookmarks/", BookmarkListView.as_view(), name="bookmark-list"),
+
+    # Admin
+    path("admin/", AdminArticleListView.as_view(), name="admin-article-list"),
+    path("admin/<uuid:id>/review/", ArticleReviewView.as_view(), name="article-review"),
+    path("admin/<uuid:id>/admin-move-review/", AdminMoveToReviewView.as_view(), name="admin-move-review"),
 ]
