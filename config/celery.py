5dc0114096de524cf59a9d2570f2aa279599f435
Nishant Kumar <nishant.kumar@qsstechnosoft.com>
2026-02-23 23:19:01 +0530

    celery settings

---
+++ config/celery.py
@@ -0,0 +1,16 @@
+import os
+from celery import Celery
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.prod")
+
+app = Celery("config")
+
+# Load settings with CELERY_ prefix
+app.config_from_object("django.conf:settings", namespace="CELERY")
+
+# Auto-discover tasks from installed apps
+app.autodiscover_tasks()
+
+@app.task(bind=True)
+def debug_task(self):
+    print(f"Request: {self.request!r}")
